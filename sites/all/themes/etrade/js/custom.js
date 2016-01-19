f030c3bf86a9fa40df6112e5314de0db79cee076
saurabhd <saurabhdhariwal.com@gmail.com>
2016-01-19 12:50:41 +0530

    changes regarding sheet issues

--- sites/all/themes/etrade/js/custom.js
+++ sites/all/themes/etrade/js/custom.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function() {
   jQuery('.navbar ul.menu').addClass("navbar-nav");
-​
   // User Register Page Header design 
   jQuery('.page-user-register .main-white-header div.container .page-header').html( "<span class='red-text'>User</span>Account" );
   jQuery('.not-logged-in.page-user .main-white-header div.container .page-header').html( "<span class='red-text'>User</span>Account" );
@@ -8,26 +7,24 @@ jQuery(document).ready(function() {
   
   // User Login Page Header design
   jQuery('#block-user-login .block-title').html( "<span class='red-text'>User</span>Login" );
-​
   /* Custom radio button & Checkbox design */
   jQuery('input[type="radio"], input[type="checkbox"]').wrap('<div class="input-rc"></div>');
   jQuery('.input-rc').append('<span class="input-rc-span"></span>');
   /* End */
-​
+
   /* Custom input file design */
   jQuery('input[type="file"]').wrap('<div class="input-file"><div class="input-file-sub"></div></div>');
   jQuery('.input-file').prepend('<span class="input-file-name">File name here</span>');
-​
   /* Get select file name value */
   jQuery('.input-file-sub input').change(function() {
   var filename = jQuery(this).val();
   jQuery(this).parent().parent().children(".input-file-name").text(filename);
   });
   /* End */
-​
+
   jQuery('#views-exposed-form-countrywise-commodities-page div.location_auto_country a').click(function() {
 	  jQuery('.view-countrywise-commodities #views-exposed-form-countrywise-commodities-page select#edit-country option').each(function(){
-​
+
 	      var val = jQuery(this).val();
 	    	var html_txt = jQuery(this).text();
 	      var html_txt = html_txt.replace(" ", "_");
@@ -38,7 +35,7 @@ jQuery(document).ready(function() {
 	        var li_html_txt = jQuery(this).text();
 	        var li_html_txt = li_html_txt.replace(" ", "_");
 	        var li_html_txt = li_html_txt.toLowerCase();
-​
+
 	        if (li_html_txt == html_txt) {
 	          var str_img = '<img src="/sites/all/themes/etrade/images/flags/' + val + '.gif" />';
 	          var new_li_val = str_img + li_html_val;
@@ -48,5 +45,4 @@ jQuery(document).ready(function() {
 	      });
   	});
  	});
-​
 });
