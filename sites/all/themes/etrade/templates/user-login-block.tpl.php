73516b1f2a5283f624c010ea0d04aead60d1a79a
saurabhd <saurabhdhariwal.com@gmail.com>
2016-01-19 11:27:06 +0530

    changes for user forget password text

--- sites/all/themes/etrade/templates/user-login-block.tpl.php
+++ sites/all/themes/etrade/templates/user-login-block.tpl.php
@@ -4,7 +4,7 @@
     <?php print $name; // Display username field ?>
     <?php print $pass; // Display Password field ?>
     <div class="links forget-pass">
-    	<a href="/user/password">Forgot Password</a>
+    	<a href="/user/password">Forgot Password?</a>
   	</div>
     <?php print $rendered; // Display hidden elements (required for successful login) ?> 
     <?php print $submit; // Display submit button ?>
