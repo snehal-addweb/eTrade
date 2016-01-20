c56db3c259f43a710eda3d5ea0a8bd31f7cdb934
saurabhd <saurabhdhariwal.com@gmail.com>
2016-01-20 17:27:40 +0530

    Wishlist tpl changes for structure

--- sites/all/themes/etrade/templates/views-view-unformatted--trade-details--page.tpl.php
+++ sites/all/themes/etrade/templates/views-view-unformatted--trade-details--page.tpl.php
@@ -32,7 +32,7 @@
 			  </div>
 				<div class="trade-title-right">
 				  <div><span class="yellow-dot"></span><span class="trade-status">Pending</span></div>
-					<div class="trade-user"><label>User:</label><span><?php print $username;?></span></div>
+					<div class="trade-user"><label>User :</label><span><?php print $username;?></span></div>
 				</div>	
 		  </div>
 			<div class="main-trade-content">
@@ -42,11 +42,11 @@
 					<div class="trade-decline">Decline</div>
 				</div>
 				<div class="trade-content-right">
-					<div class="trade-intention">Intention of Trade</div>
+					<div class="trade-intention"><label>Intention of Trade</label><span></span></div>
 					<div class="trade-qnty"><label>Quantity</label><span><?php print $qnty; ?></span></div>
 					<div class="trade-price"><label>Price</label><span><?php print $price; ?></span></div>
 					<div class="trade-port"><label>Load Port:</label><span><?php print $port; ?></span></div>
-					<div class="see-more"><a href="#">See More >></a></div>
+					<div class="see-more"><a href="#">See More</a></div>
 					<div class="add-to-wishlist"><?php print $wishlist; ?></div>
 				</div>
 			</div>
