b888db0738a8005c317c8b0186588d801bee62ba
saurabhd <saurabhdhariwal.com@gmail.com>
2016-01-20 15:55:01 +0530

    Wishlist tpl

--- sites/all/themes/etrade/templates/views-view-unformatted--trade-details--page.tpl.php
+++ sites/all/themes/etrade/templates/views-view-unformatted--trade-details--page.tpl.php
@@ -25,7 +25,8 @@
 		$port = $row['field_load_port'];
 		$wishlist = $row['ops'];
 
-		?><div class="main-title-container">
+		?><div class="main-trade-inner-container">
+			<div class="main-title-container">
 				<div class="trade-title-left">
 					<div class="trade-title"><?php print $title;?></div>
 			  </div>
@@ -33,21 +34,22 @@
 				  <div><span class="yellow-dot"></span><span class="trade-status">Pending</span></div>
 					<div class="trade-user"><label>User:</label><span><?php print $username;?></span></div>
 				</div>	
-		</div>
-		<div class="main-trade-container">
-			<div class="trade-content-left">
-				<div class="trade-img"><?php print $picture; ?></div>
-				<div class="trade-accept">Accept</div>
-				<div class="trade-decline">Decline</div>
+		  </div>
+			<div class="main-trade-content">
+				<div class="trade-content-left">
+					<div class="trade-img"><?php print $picture; ?></div>
+					<div class="trade-accept">Accept</div>
+					<div class="trade-decline">Decline</div>
+				</div>
+				<div class="trade-content-right">
+					<div class="trade-intention">Intention of Trade</div>
+					<div class="trade-qnty"><label>Quantity</label><span><?php print $qnty; ?></span></div>
+					<div class="trade-price"><label>Price</label><span><?php print $price; ?></span></div>
+					<div class="trade-port"><label>Load Port:</label><span><?php print $port; ?></span></div>
+					<div class="see-more"><a href="#">See More >></a></div>
+					<div class="add-to-wishlist"><?php print $wishlist; ?></div>
+				</div>
 			</div>
-			<div class="trade-content-right">
-				<div class="trade-intention">Intention of Trade</div>
-				<div class="trade-qnty"><label>Quantity</label><span><?php print $qnty; ?></span></div>
-				<div class="trade-price"><label>Price</label><span><?php print $price; ?></span></div>
-				<div class="trade-port"><label>Load Port:</label><span><?php print $port; ?></span></div>
-				<div class="see-more"><a href="#">See More >></a></div>
-				<div class="add-to-wishlist"><?php print $wishlist; ?></div>
-			</div>
-		</div>
+    </div>
 	<?php endforeach; ?>
 </div>
