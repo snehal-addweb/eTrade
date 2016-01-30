ff1de1cf5e72a04eecb6bbdcf93014fb83f6cfa5
saurabhd <saurabhdhariwal.com@gmail.com>
2016-01-30 19:27:52 +0530

    structure changes

--- sites/all/themes/etrade/templates/views-view-unformatted--countrywise-commodities--page.tpl.php
+++ sites/all/themes/etrade/templates/views-view-unformatted--countrywise-commodities--page.tpl.php
@@ -13,5 +13,5 @@ global $user;
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<div class="buy-main-container"><?php print views_embed_view('countrywise_commodities','block_4', $user->uid); ?></div>
-<div class="sell-main-container"><?php print views_embed_view('countrywise_commodities','block_5', $user->uid); ?></div>
+<div class="buy-main-container"><?php print views_embed_view('countrywise_commodities','block_4'); ?></div>
+<div class="sell-main-container"><?php print views_embed_view('countrywise_commodities','block_5'); ?></div>
