8c0b80c9cafbba9c05a397a80c9729f8de8b718d
saurabhd <saurabhdhariwal.com@gmail.com>
2016-01-30 16:58:28 +0530

    countrywise view structure changes

--- sites/all/themes/etrade/templates/views-view-table--filter-based-on-commodity--page.tpl.php
+++ sites/all/themes/etrade/templates/views-view-table--filter-based-on-commodity--page.tpl.php
@@ -13,5 +13,5 @@ global $user;
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<div class="buy-main-container"><?php print views_embed_view('filter_based_on_commodity','block_1', $user->uid); ?></div>
-<div class="sell-main-container"><?php print views_embed_view('filter_based_on_commodity','block_2', $user->uid); ?></div>
+<div class="buy-main-container"><?php print views_embed_view('filter_based_on_commodity','block_1', arg(2)); ?></div>
+<div class="sell-main-container"><?php print views_embed_view('filter_based_on_commodity','block_2', arg(2)); ?></div>
