aff8072bfdabb11d9fef7c98496e103cf37860aa
Saurabh Dhariwal <roshnip.addweb@gmail.com>
2016-04-09 19:12:09 +0530

    All commodities view tpl

--- sites/all/themes/etrade/templates/views-view-unformatted--trade-details--page.tpl.php
+++ sites/all/themes/etrade/templates/views-view-unformatted--trade-details--page.tpl.php
@@ -7,49 +7,11 @@
  * @ingroup views_templates
  */
 ?>
+<?php
+global $user;
+?>
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-
-<div class="main-trade-container"><?php
-	global $base_url;
-	global $user;
-	$rows = $view->style_plugin->rendered_fields;
-	foreach ($rows as $id => $row): 
-		$title = $row['title'];
-		$userinfo = user_load($row['uid']);
-		$username = $userinfo->name;
-		$picture = $row['field_commodity_picture'];
-		$qnty = $row['field_total_quantity'];
-		$price = $row['field_price_per_metric_ton'];
-		$port = $row['field_load_port'];
-		$wishlist = $row['ops'];
-
-		?><div class="main-trade-inner-container">
-			<div class="main-title-container">
-				<div class="trade-title-left">
-					<div class="trade-title"><?php print $title;?></div>
-			  </div>
-				<div class="trade-title-right">
-				  <div><span class="yellow-dot"></span><span class="trade-status">Pending</span></div>
-					<div class="trade-user"><label>User :</label><span><?php print $username;?></span></div>
-				</div>	
-		  </div>
-			<div class="main-trade-content">
-				<div class="trade-content-left">
-					<div class="trade-img"><?php print $picture; ?></div>
-					<div class="trade-accept">Accept</div>
-					<div class="trade-decline">Decline</div>
-				</div>
-				<div class="trade-content-right">
-					<div class="trade-intention"><label>Intention of Trade</label><span></span></div>
-					<div class="trade-qnty"><label>Quantity</label><span><?php print $qnty; ?></span></div>
-					<div class="trade-price"><label>Price</label><span><?php print $price; ?></span></div>
-					<div class="trade-port"><label>Load Port:</label><span><?php print $port; ?></span></div>
-					<div class="see-more"><a href="#">See More</a></div>
-					<div class="add-to-wishlist"><?php print $wishlist; ?></div>
-				</div>
-			</div>
-    </div>
-	<?php endforeach; ?>
-</div>
+<div class="buy-main-container"><?php print views_embed_view('trade_details','block_2'); ?></div>
+<div class="sell-main-container"><?php print views_embed_view('trade_details','block_3'); ?></div>
